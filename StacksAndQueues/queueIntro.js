4bd995243ce176ecf2d29d840c7167b2bf9bab06
Divs-Git <divyansh.sri258@gmail.com>
2025-07-01 20:24:48 +0530

    01-07-2025

---
+++ StacksAndQueues/queueIntro.js
@@ -0,0 +1,13 @@
+let queue = [];
+
+queue.push(10); // enqueue
+queue.push(20);
+queue.push(30);
+
+console.log(queue);
+
+queue.shift(); // dequeue
+
+console.log(queue);
+
+console.log(queue[0]); // peek/front
---
+++ StacksAndQueues/stackIntro.js
@@ -0,0 +1,13 @@
+let stack = [];
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack);
+
+stack.pop();
+
+console.log(stack);
+
+console.log(stack[stack.length - 1]); // peek
